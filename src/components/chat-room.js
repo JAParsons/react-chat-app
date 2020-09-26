f2494c8acff01eb5f4143ee1fae84a212e5075ed
JAParsons <johnparsons932@gmail.com>
2020-09-26 13:44:15 +0100

    Added chatroom component

--- src/components/chat-room.js
+++ src/components/chat-room.js
@@ -1,16 +1,42 @@
-import { firestore } from 'firebase';
-import React from 'react';
+import React, { useState } from 'react';
+import firebase from '../auth/firebase';
 import { useCollectionData } from 'react-firebase-hooks/firestore';
+import ChatMessage from './chat-message';
+
+const auth = firebase.auth();
+const firestore = firebase.firestore();
 
 const ChatRoom = () => {
   const GET_MESSAGES = firestore
-    .CollectionReference('messages')
-    .orderB('createdAt')
+    .collection('messages')
+    .orderBy('createdAt')
     .limit(32);
 
   const [messages] = useCollectionData(GET_MESSAGES, { idField: 'id' });
+  const [formValue, setFormValue] = useState('');
 
-  return <div></div>;
+  const sendMessage = async (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <>
+      <div>
+        {messages &&
+          messages.map((msg) => <ChatMessage key={msg.id} message={msg} />)}
+      </div>
+      <form onSubmit={sendMessage}>
+        <input
+          value={formValue}
+          onChange={(e) => setFormValue(e.target.value)}
+          placeholder="Aa"
+        />
+        <button type="submit" disabled={!formValue}>
+          Send
+        </button>
+      </form>
+    </>
+  );
 };
 
 export default ChatRoom;
