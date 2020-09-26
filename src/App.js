131cb157656488fba5b63c956920162727779c42
JAParsons <johnparsons932@gmail.com>
2020-09-26 13:29:52 +0100

    Login/logout functionality

--- src/App.js
+++ src/App.js
@@ -1,34 +1,29 @@
 import React from 'react';
-import firebase, { auth } from 'firebase/app';
-import { useAuthState } from 'react-firebase-hooks/auth';
-import 'firebase/firestore';
-import 'firebase/auth';
 import ChatRoom from './components/chat-room';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import firebase from './auth/firebase';
 import './App.css';
 
-firebase.initializeApp({
-  apiKey: process.env.REACT_APP_FIREBASE_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID
-});
+const auth = firebase.auth();
 
-function App() {
+const App = () => {
   const [user] = useAuthState(auth);
 
   return (
-    <div className="App">
-      <header className="App-header"></header>
+    <div>
+      <h1>React Chat App</h1>
+      <Logout />
       {user ? <ChatRoom /> : <Login />}
     </div>
   );
-}
+};
 
 const Login = () => {
-  return <button onClick={() => {}}>Login with Google</button>;
+  const loginWithGoogle = () => {
+    auth.signInWithPopup(new firebase.auth.GoogleAuthProvider());
+  };
+
+  return <button onClick={loginWithGoogle}>Login with Google</button>;
 };
 
 const Logout = () => {
